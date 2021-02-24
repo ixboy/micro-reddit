381f515de87a545fce3f555309214f05b8a20981
Nasa28 <kaluchinasaagu@gmail.com>
2021-02-24 18:12:53 +0100

    Add post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { in: 4..10 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /.*@.*/,
+    message: "Put a valid email" }
+    validates :password, presence: true, length: { in: 6..10 },  confirmation: { case_sensitive: true }
 end
